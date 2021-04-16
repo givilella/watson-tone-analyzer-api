5175036cfa869e1837b2ac0305e88df2daab78a9
Giovanni Vilella <gioatt@br.ibm.com>
2021-04-16 12:47:48 -0300

    pt response

--- controllers/api/watson-tone-analyzer.js
+++ controllers/api/watson-tone-analyzer.js
@@ -1,15 +1,29 @@
 const router = require('express').Router();
-const path = require('path');
 const fs = require('fs');
 const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
 
-router.use((req, res, next) => {
-  req.credentials = credentials;
-  return next();
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  authenticator: new IamAuthenticator({
+    apikey: `${credentials.apikey}`,
+  }),
+  serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com',
 });
 
-router.post('/test', async (req, res, next) => {
-  return res.json(req.body);
+router.post('/analyze', async (req, res) => {
+  const toneParams = {
+    toneInput: req.body,
+    contentType: 'application/json',
+    contentLanguage: 'en',
+    acceptLanguage: 'pt-br',
+  };
+  toneAnalyzer
+    .tone(toneParams)
+    .then((toneAnalysis) => res.json(toneAnalysis.result))
+    .catch((err) => res.send(err));
 });
 
 module.exports = router;
