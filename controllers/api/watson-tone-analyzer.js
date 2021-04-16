9364d3412366aec2cac2ce263e87e935bfad0f40
Giovanni Vilella <gioatt@br.ibm.com>
2021-04-15 21:39:48 -0300

    basic express setup

--- controllers/api/watson-tone-analyzer.js
+++ controllers/api/watson-tone-analyzer.js
@@ -1,7 +1,15 @@
-const router = require('express').Router;
+const router = require('express').Router();
+const path = require('path');
+const fs = require('fs');
+const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
 
-router.post('/test', (req, res, next) => {
-  
-})
+router.use((req, res, next) => {
+  req.credentials = credentials;
+  return next();
+});
+
+router.post('/test', async (req, res, next) => {
+  return res.json(req.body);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const logger = require('morgan');
 
 const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(logger('tiny'));
 app.use(express.json());
 app.use('/api', require('./controllers/api/index'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
-})
+});
